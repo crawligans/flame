6256f156a5042318b26ffbd2a59e4aa43e63a188
wu000168 <wu000168@umn.edu>
2023-05-03 14:10:15 -0400

    only fold on success

--- app/src/main/java/cis5550/flame/FlameContextImpl.java
+++ app/src/main/java/cis5550/flame/FlameContextImpl.java
@@ -118,7 +118,7 @@ public abstract class FlameContextImpl implements FlameContext, Serializable {
     if (partitions == null) {
       partitions = partitioner.assignPartitions();
     }
-    List<Response> responses = partitions.parallelStream().map(part -> {
+    Stream<Response> responses = partitions.parallelStream().map(part -> {
       try {
         return HTTP.doRequest("POST", new URL(
             "http://" + part.assignedFlameWorker + operation.route + "?" + String.join("&",
@@ -139,10 +139,12 @@ public abstract class FlameContextImpl implements FlameContext, Serializable {
       } else {
         logger.error(new String(res.body()));
       }
-    }).toList();
+    });
     return switch (operation) {
-      case FOLD -> responses.stream().map(res -> new String(res.body(), StandardCharsets.UTF_8))
-          .reduce((String) args[0], ((TwoStringsToString) args[1])::op);
+      case FOLD ->
+          responses.filter(res -> 200 <= res.statusCode() && res.statusCode() < 300)
+              .map(res -> new String(res.body(), StandardCharsets.UTF_8))
+              .reduce((String) args[0], ((TwoStringsToString) args[1])::op);
       default -> outputTable;
     };
   }
